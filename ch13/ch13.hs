8894ce50b3cfb4e3ef36fbdb776cc0ac0e424edb
Israa Elwanni <israa.el_wanni.4732@student.lu.se>
2024-08-17 13:29:17 +0200

    notes from ch 13.1 - 13.5

---
+++ ch13/ch13.hs
@@ -0,0 +1,54 @@
+import Control.Applicative 
+import Data.Char
+
+newtype Parser a = P (String -> [(a,String)])
+
+parse :: Parser a -> String -> [(a,String)] 
+parse (P p) inp = p inp
+
+item :: Parser Char
+item = P (\inp -> case inp of
+            [] -> []
+            (x:xs) -> [(x,xs)])
+
+instance Functor Parser where
+    -- fmap :: (a -> b) -> Parser a -> Parser b 
+    fmap g p = P (\inp -> case parse p inp of
+                [] -> []
+                [(v,out)] -> [(g v, out)])
+
+instance Applicative Parser where 
+    -- pure :: a -> Parser a
+    pure v = P (\inp -> [(v,inp)])
+
+    -- <*> :: Parser (a -> b) -> Parser a -> Parser b 
+    pg <*> px = P (\inp -> case parse pg inp of
+                    [] -> []
+                    [(g,out)] -> parse (fmap g px) out)
+
+three :: Parser (Char,Char)
+three = pure g <*> item <*> item <*> item
+    where 
+        g x y z = (x,z)
+
+-- three :: Parser (Char,Char) 
+-- three = do x <- item item z <- item return (x,z)
+
+instance Monad Parser where
+    -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b 
+    p >>= f = P (\inp -> case parse p inp of
+            [] -> []
+            [(v,out)] -> parse (f v) out)
+
+class Applicative f => Alternative f where 
+    empty :: f a
+    (<|>) :: f a -> f a -> f a
+
+instance Main.Alternative Parser where 
+    empty :: Parser a
+    empty = P (\inp -> [])
+
+    (<|>) :: Parser a -> Parser a -> Parser a 
+    p <|> q = P (\inp -> case parse p inp of
+                [] -> parse q inp 
+                [(v,out)] -> [(v,out)])
